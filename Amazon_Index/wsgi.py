fae61fc9ebebff84fa37b55d9a4ed2777842a12b
wxppt <wx_ppt@126.com>
2014-05-27 21:42:52 +0800

    New Project...

---
+++ Amazon_Index/wsgi.py
@@ -0,0 +1,14 @@
+"""
+WSGI config for Amazon_Index project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
+"""
+
+import os
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Amazon_Index.settings")
+
+from django.core.wsgi import get_wsgi_application
+application = get_wsgi_application()
