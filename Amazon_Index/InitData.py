8a3ef68587122e48c92a88f6b289720a31ca9044
wxppt <wx_ppt@126.com>
2014-05-31 11:18:56 +0800

    ...

--- Amazon_Index/InitData.py
+++ Amazon_Index/InitData.py
@@ -5,6 +5,7 @@ from Trendata import *
 def calc_cate_sales_turnover():
     result_dict = {}
     cate_list = get_category_list()
+
     for single_cate in cate_list:
         result_dict[single_cate] = {'sales': 0, 'turnover': 0}
         asin_list = get_category_asin_list(single_cate)
@@ -20,10 +21,15 @@ def calc_cate_sales_turnover():
                     result_dict[single_cate]['turnover'] += price
             else:
                 print 'Error: Empty Commodity Data'
-        print str(result_dict)
+        print single_cate + '--' + str(result_dict[single_cate]['sales']) + '--' + str(result_dict[single_cate]['turnover'])
+        file_object = open('tmp.txt','w+')
+        file_object.write(single_cate + '--' + str(result_dict[single_cate]['sales']) + '--' + str(result_dict[single_cate]['turnover']) + '\r\n')
+        file_object.close()
     print '==========================RESULT=========================='
     print str(result_dict)
-
+    file_object = open('turnover.txt','w')
+    file_object.write(str(result_dict))
+    file_object.close()
 
 if __name__ == "__main__":
     calc_cate_sales_turnover()
